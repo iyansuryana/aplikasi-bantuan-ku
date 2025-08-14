45cdc4ea97c55d52c1e6e51e76b85090b1a798a2
iyansuryana <iyansuryana999999@gmail.com>
2025-08-14 07:23:28 +0700

    Add .next/static/chunks/src_app_new-assessment_page_tsx_a993527f._.js 1755131009715

---
+++ .next/static/chunks/src_app_new-assessment_page_tsx_a993527f._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@firebase_auth_dist_esm_4a0936ab._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm_bf7383cb.js",
+  "static/chunks/node_modules_d58b38fe._.js",
+  "static/chunks/src_a6e7cf74._.js"
+],
+    source: "dynamic"
+});
